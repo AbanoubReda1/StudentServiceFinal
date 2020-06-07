5e4aca708d0d3be9fc651040d881e0c7868e6e69
AbanoubReda1 <54524800+AbanoubReda1@users.noreply.github.com>
2020-06-07 14:39:24 +0200

    add syllbaus &projects

---
+++ Controllers/ProjectStdController.cs
@@ -0,0 +1,60 @@
+﻿using StudentService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentService.Controllers
+{
+    public class ProjectStdController : Controller
+    {
+        // GET: ProjectStd
+        private StudentServiceEntities db = new StudentServiceEntities();
+        public ActionResult Index()
+        {
+           
+            ViewBag.projectlist = new SelectList(db.Projects, "ProjectID", "ProjectName");
+            var projects = db.Projects.Where(a=>a.ProjectName==a.Professor);
+            return View(projects.ToList());
+        }
+        [HttpPost]
+        public ActionResult Index(int? projectlist)
+        {
+           
+            ViewBag.projectlist = new SelectList(db.Projects, "ProjectID", "ProjectName");
+
+            var project = db.Projects.Where(a => a.ProjectID == projectlist).ToList();
+
+            return View(project.ToList());
+        }
+
+
+        public ActionResult All()
+        {
+
+            
+            var projects = db.Projects.ToList();
+            return View(projects.ToList());
+        }
+        public ActionResult DownloadFile(string filePath)
+        {
+            string fullName = Server.MapPath("" + filePath);
+
+            byte[] fileBytes = GetFile(fullName);
+            return File(
+                fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
+        }
+
+        byte[] GetFile(string s)
+        {
+            System.IO.FileStream fs = System.IO.File.OpenRead(s);
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new System.IO.IOException(s);
+            return data;
+        }
+
+    }
+}
---
+++ Controllers/StudeCourseController.cs
@@ -0,0 +1,60 @@
+﻿using StudentService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentService.Controllers
+{
+    public class StudeCourseController : Controller
+    {
+        // GET: StudeCourse
+        StudentServiceEntities db = new StudentServiceEntities();
+        public ActionResult Index()
+            {
+                List<Department> Departmentlist = db.Departments.ToList();
+                ViewBag.Departmentlist = new SelectList(Departmentlist, "DepartmentCode", "DepartmentName");
+
+            var courses = db.Courses.Where(a => a.CourseCode == a.DepartmentCode);
+
+                return View(courses);
+            }
+
+        [HttpPost]
+        public ActionResult Index(string CourseCode)
+        {
+            List<Department> Departmentlist = db.Departments.ToList();
+            ViewBag.Departmentlist = new SelectList(Departmentlist, "DepartmentCode", "DepartmentName");
+         
+            var courses = db.Courses.Where(a => a.CourseTitle == CourseCode).ToList();
+
+            return View(courses);
+        }
+        public JsonResult GetCourse(string DepartmentCode)
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+            List<Course> courselist = db.Courses.Where(x => x.DepartmentCode == DepartmentCode).ToList();
+
+            return Json(courselist, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult DownloadFile(string filePath)
+        {
+ string fullName = Server.MapPath("" + filePath);
+
+            byte[] fileBytes = GetFile(fullName);
+            return File(
+                fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, filePath);
+        }
+
+        byte[] GetFile(string s)
+        {
+            System.IO.FileStream fs = System.IO.File.OpenRead(s);
+            byte[] data = new byte[fs.Length];
+            int br = fs.Read(data, 0, data.Length);
+            if (br != fs.Length)
+                throw new System.IO.IOException(s);
+            return data;
+        }
+    }
+}
