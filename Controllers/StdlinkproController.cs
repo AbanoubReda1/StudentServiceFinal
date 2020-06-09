850c4db775cd28b22a7387c5523cfabb9b849082
AbanoubReda1 <54524800+AbanoubReda1@users.noreply.github.com>
2020-06-09 21:14:14 +0200

    update

---
+++ Controllers/StdlinkproController.cs
@@ -0,0 +1,27 @@
+﻿using StudentService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentService.Controllers
+{
+    public class StdlinkproController : Controller
+    {
+        // GET: Stdlinkpro
+        private StudentServiceEntities db = new StudentServiceEntities();
+        public ActionResult Index()
+        {
+
+            var courses = db.Links.ToList();
+            return View(courses.ToList());
+        }
+        public ActionResult Del(int id)
+        {
+
+            var courses = db.Links.Where(a => a.LinkID == id);
+            return View(courses.ToList());
+        }
+    }
+}
